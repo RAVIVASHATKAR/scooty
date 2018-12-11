d445008321c05d6c303df75c4ecae88dc4a229d8
Carl Tablante <carl.q.tablante@gmail.com>
2018-12-11 19:14:15 +0800

    Add current location markers and scooter markers

--- src/app/Home/index.js
+++ src/app/Home/index.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react'
 import Map from './map'
+import ScooterLocations from './scooters';
+import currentLocation from '../lib/geolocation';
 
 class Home extends Component {
+  constructor() {
+    super()
+    this.state = {
+      scooterLocations: ScooterLocations,
+      currentLocation: currentLocation(),
+    }
+  }
+
   render() {
+    const { scooterLocations }= this.state
+    const { currentLocation } = this.state
+
     return (
       <div className="home">
         <Map
+          {...{ scooterLocations }}
+          center={currentLocation}
           googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyBuOsOYis8mssY8a0FL-V859ol2Ktp9jPU"
           loadingElement={<div style={{width: 500, height: 500}} />}
           mapElement={<div style={{width: 500, height: 500}} />}
--- src/app/Home/map.js
+++ src/app/Home/map.js
@@ -1,18 +1,49 @@
 import React, { Component } from 'react';
-import currentLocation from '../lib/geolocation';
 import { withGoogleMap, withScriptjs, GoogleMap, Marker } from 'react-google-maps';
 
 class Map extends Component {
+  renderCurrentPositionMarker = () => {
+    const { center } = this.props
+
+    return(
+      <Marker
+        key={"currentPosition"}
+        position={center}
+        options={{icon: "http://maps.google.com/mapfiles/ms/icons/blue-dot.png" }}
+      />
+    )
+  }
+
+  renderMarkers = () => {
+    let markers = [];
+    const { scooterLocations } = this.props
+
+    markers = scooterLocations.map((pos, i) => {
+      return(
+        <Marker
+          key={i}
+          position={pos}
+        />
+      )
+    })
+
+    markers.push(this.renderCurrentPositionMarker())
+    return markers
+  }
+
   render() {
     const SINGAPORE_CENTER = { lat: 1.3521, lng: 103.8198 }
     const center = this.props.center || SINGAPORE_CENTER
 
+    console.log(center);
+    if (center == null) { return }
+
     return(
       <GoogleMap
-        defaultZoom={12}
+        defaultZoom={11}
         defaultCenter={center}
       >
-        <Marker position={center} />
+        { this.renderMarkers() }
       </GoogleMap>
     )
   }
